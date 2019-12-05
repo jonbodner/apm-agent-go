6a9cb336b249405659275a523dc297cf6049287c
Jon Bodner <jon@bodnerfamily.com>
2019-12-05 12:43:29 -0500

    Support unwrap serrors (#1)
    * feat: add support for the Unwrap interface and the serrors package we're going to open source
    
    * fix: format code, remove commented-out function
    
    * fix: make test test something
    
    * fix: remove unrelated changes

--- error_go113_test.go
+++ error_go113_test.go
@@ -21,12 +21,15 @@ package apm_test
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
+
+	realErr "errors"
 
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
+	"go.elastic.co/apm/apmtest"
 	"go.elastic.co/apm/transport/transporttest"
 )
 
@@ -44,4 +47,91 @@ func TestErrorCauseUnwrap(t *testing.T) {
 
 	require.Len(t, payloads.Errors[0].Exception.Cause, 1)
 	assert.Equal(t, "cause", payloads.Errors[0].Exception.Cause[0].Message)
+}
+
+// StackErr wraps an error with the stack location where the error occurred. Use the WithStack
+// function to create a StackErr. There can only be one StackErr in the error chain, ideally at
+// the root error location.
+type StackErr struct {
+	Err   error
+	trace []uintptr
+}
+
+func (se StackErr) StackTrace() *runtime.Frames {
+	frames := runtime.CallersFrames(se.trace)
+	return frames
+}
+
+// WithStack takes in an error and returns an error wrapped in a StackErr with the location where
+// an error was first created or returned from third-party code. If there is already a StackErr
+// in the error chain, WithStack returns the passed-in error.
+func WithStack(err error) error {
+	var se StackErr
+	if realErr.As(err, &se) {
+		return err
+	}
+	pc := make([]uintptr, 20)
+	n := runtime.Callers(2, pc)
+	pc = pc[:n]
+	return StackErr{
+		Err:   err,
+		trace: pc,
+	}
+}
+
+// Unwrap exposes the error wrapped by StackErr
+func (se StackErr) Unwrap() error {
+	return se.Err
+}
+
+// Error is the marker interface for an error, it returns the wrapped error or an empty string if there is no
+// wrapped error
+func (se StackErr) Error() string {
+	if se.Err == nil {
+		return ""
+	}
+	return se.Err.Error()
+}
+
+func TestMyStackErr(t *testing.T) {
+	e := realErr.New("This is a test")
+	e = WithStack(e)
+	tracer := apmtest.NewRecordingTracer()
+	defer tracer.Close()
+
+	e2 := tracer.NewError(e)
+	e2.Send()
+	tracer.Flush(nil)
+	stacktrace := tracer.RecorderTransport.Payloads().Errors[0].Exception.Stacktrace
+	if len(stacktrace) != 3 {
+		t.Fatal("Expected 3 elements in stack")
+	}
+	data := []struct {
+		file     string
+		module   string
+		function string
+	}{
+		{
+			file:     "error_go113_test.go",
+			module:   "go.elastic.co/apm_test",
+			function: "TestMyStackErr",
+		},
+		{
+			file:     "testing.go",
+			module:   "testing",
+			function: "tRunner",
+		},
+	}
+	// we're checking the first two only, because the third is a platform-specific assembly file
+	for k, v := range data {
+		if stacktrace[k].File != v.file {
+			t.Errorf("unexpected file for %d: %s", k, stacktrace[k].File)
+		}
+		if stacktrace[k].Module != v.module {
+			t.Errorf("unexpected module for %d: %s", k, stacktrace[k].Module)
+		}
+		if stacktrace[k].Function != v.function {
+			t.Errorf("unexpected function for %d: %s", k, stacktrace[k].Function)
+		}
+	}
 }
